777461160fe7b565515244725f3d227386c49e34
shannon_p <snp1284kasey@aol.com>
2021-09-02 18:00:17 -0400

    trying to fix errors in scripts

--- index.js
+++ index.js
@@ -1,3 +1,119 @@
-const Prompt = require("./lib/Prompts");
+// node modules
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateTeamProfiles = require("./src/page-template");
 
-new Prompt().promptUser();
+// lib modules
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const employeesData = [];
+
+
+const questions = async () => {
+    const employeeAnswers = await inquirer
+    .prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is your employee's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your employee's Id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your employee's email?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is your employee's role?",
+        choices: ["Manager", "Engineer","Intern"],
+    }
+])
+    if (employeeAnswers.role === "Manager") {
+        const officeNumberAnswer = await inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the manager's office number?",
+            },
+        ])
+        // creates a new Manager object and is added to the employees array
+        const roleOfManager = new Manager(
+            employeeAnswers.name,
+            employeeAnswers.id,
+            employeeAnswers.email,
+            officeNumberAnswer.officeNumber
+        );
+        employeesData.push(roleOfManager);
+    }
+    else if (employeeAnswers.role === "Engineer") {
+        const githubUserName = await inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "github",
+                message: "What is the engineer's github username?",
+            },
+        ])
+        // creates a new Engineer object and is added to the employees array
+        const roleOfEngineer = new Engineer(
+            employeeAnswers.name,
+            employeeAnswers.id,
+            employeeAnswers.email,
+            githubUserName.github
+        );
+        employeesData.push(roleOfEngineer);
+    }
+    else if (employeeAnswers.role === "Intern") {
+        const internSchool = await inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "school",
+                message: "What University/College does the intern attend?",
+            },
+        ])
+        // creates a new Intern object and is added to the employees array
+        const roleOfIntern = new Intern(
+            employeeAnswers.name,
+            employeeAnswers.id,
+            employeeAnswers.email,
+            internSchool.school
+        );
+        employeesData.push(roleOfIntern);
+    }
+};
+
+async function promptQuestions() {
+    await questions()
+
+        const addMoreRoles = await inquirer
+        .prompt([
+        {
+            type: "list",
+            name: "addRoles",
+            message: "What would you like to do?",
+            choices: ["Add another role", "Generate Profiles"]
+        }
+        ])
+
+        if(addMoreRoles.addRoles === "Add another role") {
+            return promptQuestions();
+        }
+        return generateProfiles();
+}
+
+promptQuestions();
+
+function generateProfiles () {
+    console.log("Team Profiles have been created! Check out index.html to see the output!");
+    fs.writeFileSync("./dist/index.html", generateTeamProfiles(employeesData));
+}
--- src/page-template.js
+++ src/page-template.js
@@ -1,29 +1,85 @@
-const userInput = employeeData => {
-    i
-
-const generateEmployeeProfile = templateData => {
+const generateTeamProfiles = (teamProfile) => {
+    
+const generateManagerProfile = (manager) => {
     return `
-    <body>
-    <div class="header-wrapper">
-    <h1>Team Profiles</h1>
+    <div class="card employee-card">
+    <div class="card-header">
+    <h2 class="card-title">${manager.getName()}</h2>
+    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
     </div>
-    <div class="grid"> 
-    ${templateData.map((employee) => {
-        return `
-        <div>
-        <h2>${employee.getName()}</h2>
-        <h3>${employee.getRole()}</h3>
-        <p>${employee.getId()}</p>
-        <p>Email: <a href="mailto:https://${employee.getEmail()}">${employee.getEmail}</a></p>
-        <p>${userInput(employee)}</p>
-        </div>
-        `
-    }).join(' ')}
+    <div class="card-body">
+    <ul class=list-group">
+    <li class="list-group-item">ID: ${manager.getId()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail}</a></li>
+    <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+    </ul>
     </div>
-    </body>`
+    </div>
+    `;
 };
 
-module.exports = teamProfile => {
+const generateEngineerProfile = (engineer) => {
+    return `
+    <div class="card employee-card">
+    <div class="card-header">
+    <h2 class="card-title">${engineer.getName()}</h2>
+    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+    <ul class=list-group">
+    <li class="list-group-item">ID: ${engineer.getId()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail}</a></li>
+    <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGitHub()}" target="_blank">${engineer.getGitHub()}</a></li>
+    </ul>
+    </div>
+    </div>
+    `;
+};
+
+const generateInternProfile = (intern) => {
+    return `
+    <div class="card employee-card">
+    <div class="card-header">
+    <h2 class="card-title">${intern.getName()}</h2>
+    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+    <ul class=list-group">
+    <li class="list-group-item">ID: ${intern.getId()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${engineer.getEmail()}</a></li>
+    <li class="list-group-item">Office Number: ${intern.getSchool()}</li>
+    </ul>
+    </div>
+    </div>
+    `;
+};
+
+const userInput = [];
+
+userInput.push(
+    teamProfile
+    .filter((employee) => employee.getRole() === "Manager")
+    .map((manager) => generateManagerProfile(manager))
+);
+userInput.push(
+    teamProfile
+    .filter((employee) => employee.getRole() === "Engineer")
+    .map((engineer) => generateEngineerProfile(engineer))
+    .join("")
+);
+userInput.push(
+    teamProfile
+    .filter((employee) => employee.getRole() === "Intern")
+    .map((intern) => generateInternProfile(intern))
+    .join("")
+);
+
+return userInput.join("");
+
+};
+
+module.exports = (teamProfile) => {
+    console.log(teamProfile);
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -31,10 +87,10 @@ module.exports = teamProfile => {
             <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
+    <title>Team Profile</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+     <link rel="stylesheet" href="style.css">
         </head>
             <body>
             <div class="container-fluid">
@@ -47,11 +103,11 @@ module.exports = teamProfile => {
             <div class="container">
             <div class="row">
             <div class="team-area col-12 d-flex justify-content-center">
-            ${generateEmployeeProfile(teamProfile)}
+            ${generateTeamProfiles(teamProfile)}
             </div>
             </div>
             </div>
             </body>
     </html>
-    `
+    `;
 };
